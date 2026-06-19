29726a19053edb46518f20a9642b6e5eb9678d8c
Akshay Shelke <dev.akshayshelke@gmail.com>
2026-06-19 16:13:36 +0530

    scroll on top

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Services from './pages/Services'
@@ -7,9 +8,21 @@ import Blog from './pages/Blog'
 import BlogPost from './pages/BlogPost'
 import WhatsAppFloat from './components/WhatsAppFloat'
 
+function ScrollToTop() {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'auto' })
+  }, [pathname])
+
+  return null
+}
+
 export default function App() {
   return (
     <>
+      <ScrollToTop />
+
       <Routes>
         <Route path="/"         element={<Home />} />
         <Route path="/about"    element={<About />} />
